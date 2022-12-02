3bad972fadf3a26fe849a86793b0b2ae83236e9f
James <jameswmorris@gmail.com>
2022-12-02 18:06:53 +0900

    refactoring

--- src/pages/AboutPage.js
+++ src/pages/AboutPage.js
@@ -2,7 +2,7 @@ import Card from '../components/shared/Card'
 import { Link } from 'react-router-dom'
 
 function AboutPage() {
-  
+   
     return (
         <Card>
             <div className="about">
